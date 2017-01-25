df3e31b49ac13c26233bcf50d072adefbdddb9d0
codeamac16 <codeamac16@gmail.com>
2017-01-25 13:22:12 -0600

    i LOVE Java Script so much

--- post-it.js
+++ post-it.js
@@ -12,24 +12,49 @@ var Board = function( selector ) {
   initialize();
 };
 
-var PostIt = function() {
+var PostIt = function(number) {
   // Aquí va el código relacionado con un post-it
-  $("#board").dblclick(function(e){
-    e.stopPropagation();
-    post = $('#master').clone().css({ 'left': e.pageX, 'top': e.pageY }).appendTo('#board');
-    console.log(e.pageX, e.pageY);
-    console.log(post);
-    $(post).draggable({
-    }); 
-  });
+  this.post = '<div class="post-it" id="'+number+'"><div class="header"><a href="#">X</a></div><div contenteditable="true" class="content">...</div></div>';
 };
 
 $(function() {
-  // Esta es la fucnión que correrá cuando este listo el DOM 
-  $('#master').draggable({});
-  var popi = new Board('#board');
-  PostIt();
-
+  // Esta es la fucnión que correrá cuando este listo el DOM
+  num = 1; 
+  $('#0').draggable({handle: '.header'});
+  CreatePostIt();
+  PasstoFront();
+  StopCreating();
+  DeletePost();
 });
 
+var CreatePostIt = function(){
+  $("#board").dblclick(function(e){
+    newPostIt = new PostIt(num);
+    dragPost = $(newPostIt.post).css({ 'left': e.pageX, 'top': e.pageY }).appendTo('#board');
+    $(dragPost).draggable({handle: '.header'}); 
+    num += 1;
+  });
+}
+
+
+var PasstoFront = function(){
+  $('#board').on('click', '.post-it', function () {
+      console.log('ya estoy en el id de cada post')
+      $(this).css('z-index', 2);
+      $(this).siblings('div').css('z-index', 1);
+  });
+}
+
+var StopCreating = function(){
+  $('#board').on('dblclick', '.post-it', function(event){
+      event.stopPropagation();
+  });
+}
+
+var DeletePost = function(e){
+  $('#board').on('click', 'a', function(){
+    parent = this.parentElement.parentElement
+    parent.remove();
+  });
+}
 
